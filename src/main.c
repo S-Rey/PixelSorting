ec206cf06eedc70a0fe7b537bd13b7685f01f7ae
S-Rey <samuel.rey@epfl.ch>
2017-08-26 21:38:20 +0200

    add argument with getopt

--- src/main.c
+++ src/main.c
@@ -1,19 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "read_write.h"
+#include <getopt.h>
+//#include "read_write.h"
 
 
-int main(int argc, char *argv[]) {
-	if(argc != 3) {
-		printf("Wrong number of arguments\n");
-		return 1;
-	}
-	printf("Hello\n");
-//	read_png_file("");
+int main(int argc, char **argv) 
+{
+	char *input = NULL, *output = NULL;		// input and output file
+	double angle;							// the angle used for pixelsorting
+	double black_threshold;					// 
 
-//  read_png_file(argv[1]);
-//  process_png_file();
-//  write_png_file(argv[2]);
+	int index = 0;
+	int c;
+
+	opterr = 0;
+
+
+	while ((c = getopt (argc, argv, "::i:o:a:")) != -1) {
+		switch (c) {
+			case 'a':
+				angle = atof(optarg);
+				break;
+			case 'i':
+				input = optarg;
+				break;
+			case 'o':
+				input = optarg;							
+				break;
+			case ':':
+				++index;
+				printf("%d", index);
+			case '?':
+				if (optopt == 'a' || optopt == 'i' || optopt == 'o')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+        return 1;
+			default:
+				abort();
+		}
+	}	
+
+	printf("-i %s -o %s -a %f\n", input, output, angle);
+
 
 	return 0;
 }
