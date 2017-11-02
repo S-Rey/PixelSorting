0679309db978fa7155ff9dd8e6e4a1ca58b47154
S-Rey <samuel.rey@epfl.ch>
2017-11-02 08:43:51 +0100

    Remove error files, because not used yet

--- src/error.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file error.c
- * @brief filesystem error messages
- */
-
-const char * const ERR_MESSAGES[] = {
-	"", // no error
-	"Not the correct number of parameter passed to the program."
-};
